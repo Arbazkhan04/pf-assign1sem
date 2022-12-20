1ed3cfcf38795f87dc0cfc372b30aedba4014dd3
Arbaz <arbaz582021@gmail.com>
2022-12-20 18:10:23 +0500

    add

--- 3reWeekAssign/task1.cpp
+++ 3reWeekAssign/task1.cpp
@@ -12,7 +12,7 @@ rollNumer=10;
 aggregrate=58.23;
 grade='A';
 cout<<"Your name is"<<name<<endl;
-cout<<"your rollNumber is "<<rollNumer<<endl;;
+cout<<"your rollNumber is "<<rollNumer<<endl;
 cout<<"your grade is "<<grade<<endl;;
 cout<<"your aggregrate is "<<aggregrate<<endl;
 }
