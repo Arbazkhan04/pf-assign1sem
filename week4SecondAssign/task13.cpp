5bda442573e50b7af6deb15a8b71917f4078ca2d
arbaz khan <arbaz582021@gmail.com>
2022-12-31 23:39:22 -0800

    add

--- week4SecondAssign/task13.cpp
+++ week4SecondAssign/task13.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
+
 int main()
 {
  int workingMinutePerDay=63,holidaysMinutePerDays=127,totalDaysInYear=365,holidays,normGameTomhas=30000;
@@ -8,7 +10,15 @@ int main()
  int WorkingDays=totalDaysInYear-holidays;
  int timeForGames=(WorkingDays*workingMinutePerDay)+(holidays*holidaysMinutePerDays);
  int normGameDifference=normGameTomhas-timeForGames;
- cout<<normGameDifference;
+//  cout<<normGameDifference<<endl;
+ int hourForNormDays=normGameDifference/60;
+//  cout<<hourForNormDays<<endl;
+ float pointingvaluesHourForNormDays=(normGameDifference/60.00);
+//  cout<<pointingvaluesHourForNormDays<<endl;
+ float minuteForNormsDays=round((pointingvaluesHourForNormDays-hourForNormDays)*60.00);
+//  cout<<minuteForNormsDays;
+ cout<<hourForNormDays  << " Hour " <<minuteForNormsDays<< " Minute ";
+
 
 
 
