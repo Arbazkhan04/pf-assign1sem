2b2b40ed622869aef907f45d66b7fe270e7e11c3
Arbaz <arbaz582021@gmail.com>
2023-01-19 10:16:19 +0500

    add

---
+++ pfLab7/task13.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void printPercentagee(int number);
+int main()
+{
+
+}
+void printPercentagee(int number)
+{
+    while(number>0)
+    {
+        if(number<200)
+        {
+            
+        }
+    }
+}
