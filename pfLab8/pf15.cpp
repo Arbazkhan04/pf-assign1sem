b2742070e2c78c2f6a6e294b796d4aca13f41d2e
Arbaz <arbaz582021@gmail.com>
2023-01-31 16:26:03 +0500

    add

--- pfLab8/pf15.cpp
+++ pfLab8/pf15.cpp
@@ -8,23 +8,33 @@ int main()
     cout<<"Enter second array size";
     cin>>secondSize;
     int arr1[firstSize];
-    int arr2[secondSize];
+    int arr2[secondSize]; 
     for(int i=0;i<firstSize;i++)
     {
-        cout<<"Enter no. for fisrt array ";
+        cout<<"Enter no. for fisrt array for only size having two";
         cin>>arr1[i];
     }
-     for(int j=0;j<firstSize;j++)
+     for(int j=0;j<secondSize;j++)
     {
         cout<<"Enter no. for fisrt array ";
         cin>>arr2[j];
     }
-    int totalsize=sizeof(arr1)/sizeof(arr1[0])+sizeof(arr2)/sizeof(arr2[0]);
-    for(int i=0;i<=totalsize;i++)
+    int totalsize=sizeof(arr2)/sizeof(arr2[0]);
+    // cout<<totalsize;
+    int thirdarray[totalsize];
+    thirdarray[0]=arr1[0];
+    thirdarray[(totalsize-1)+2]=arr1[2];
+      int m=0;
+    for(int i=1;i<totalsize;i++)
     {
-        
+      
+     thirdarray[i]=arr2[m];
+     m++;
     }
-    
+    for(int i=0;i<totalsize+2;i++)
+    {
+      cout<<thirdarray[i]<<" ";
+    }
 
     
 }
