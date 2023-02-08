ca3c460d1f1068bcfa03a49d09adf4013d3577e7
arbaz khan <arbaz582021@gmail.com>
2023-02-08 11:27:21 -0800

    add

--- pd9/task4First.cpp
+++ pd9/task4First.cpp
@@ -1,35 +1,42 @@
 // Write a program that takes an array of numbers as input from the user and then prints
 // "Boom!" if the digit 7 appears in the array. Otherwise, print "there is no 7 in the array".
 #include<iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-    int flag=false;
+    bool flag=false;
  int size;
  cout<<"Enter size fo array ";
  cin>>size;
- int arr[size];
+ string arr[size];
  for(int i=0;i<size;i++)   
  {
     cout<<"Enter a number ";
     cin>>arr[i];
     
  }
-  for(int i=0;i<size;i++)   
+ string tem;
+ for(int i=0;i<size;i++)
  {
-    if(arr[i]==7)
-    {
+    // int j=0;
+     tem=arr[i];
+     for(int j=0;tem[j]!='\0';j++)
+     {
+       if(tem[j]=='7')
+       {
         flag=true;
-        break;
-    }
-    else
-    {
-        flag=false;
-    }
+       }
+     }
+    
  }
  if(flag==true)
- cout<<"Boom";
- else if(flag==false)
- cout<<"Ther is no number 7 in array";
- 
+ {
+    cout<<"Boom";
+ }
+ else 
+ {
+    cout<<"Not Bomm";
+ }
 }
+ 
---
+++ pd9/test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  string namee="abc";
+  int i=0;
+  while(namee[i]!='\0')
+  {
+    cout<<namee[i];
+    i++;
+  }
+}
